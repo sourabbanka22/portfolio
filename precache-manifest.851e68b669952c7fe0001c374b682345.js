c2af65bc65e565abc27923e1cd3684a42a3a310d
sourabbanka22 <sourabbanka22@gmail.com>
2020-01-27 23:27:45 +0530

    Updates

---
+++ precache-manifest.851e68b669952c7fe0001c374b682345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36433f43aef968ed2f468cbe54eaf195",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e35101e03f28e606eb24",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "596a10aa04d0c7984573",
+    "url": "/portfolio/static/js/2.536684dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.536684dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e35101e03f28e606eb24",
+    "url": "/portfolio/static/js/main.8b0526b4.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
